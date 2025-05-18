4360111c9974989b8090d2784d9aaaa297a9c45b
Olirniy <divvushonn@yandex.ru>
2025-05-18 15:40:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def add_numbers(a, b):
     return a + b
 
+def safe_add_numbers():
+    try:
+        num1 = float(input("Введите первое число: "))
+        num2 = float(input("Введите второе число: "))
+        result = add_numbers(num1, num2)
+        print(f"Результат сложения: {result}")
+    except ValueError:
+        print("Ошибка: введите числа!")
+
 # Пример использования
 num1 = 5
 num2 = 3
